e8af8aaec14733163f2cb54cef9db028ab0ea7ef
aakvatech <info@aakvatech.com>
2023-01-28 20:13:09 +0000

    feat: VFDPlus Settings

---
+++ vfd_providers/vfd_settings/doctype/vfdplus_settings/vfdplus_settings.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Aakvatech Limited and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class VFDPlusSettings(Document):
+	pass
