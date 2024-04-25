274b5cda31dda7a42a387eb874bdb977807795a3
aakvatech <info@aakvatech.com>
2024-04-26 01:10:29 +0300

    refactor: Streamlined for TotalVFD addition

--- vfd_providers/utils/utils.py
+++ vfd_providers/utils/utils.py
@@ -4,7 +4,7 @@ from frappe import _
 
 
 @frappe.whitelist()
-def generate_tra_vfd(docname, sinv_doc=None):
+def generate_tra_vfd(docname, sinv_doc=None, method="POST"):
     if not sinv_doc:
         sinv_doc = frappe.get_doc("Sales Invoice", docname)
     if sinv_doc.is_not_vfd_invoice or sinv_doc.vfd_status in ["Sent", "Success"]:
@@ -23,19 +23,18 @@ def generate_tra_vfd(docname, sinv_doc=None):
         from vfd_providers.vfd_providers.doctype.vfdplus_settings.vfdplus_settings import (
             post_fiscal_receipt,
         )
-
-        post_fiscal_receipt(sinv_doc)
-
     elif vfd_provider.name == "TotalVFD":
         from vfd_providers.vfd_providers.doctype.total_vfd_setting.total_vfd_setting import (
             post_fiscal_receipt,
         )
+    else:
+        frappe.throw(_("VFD Provider not supported"))
 
-        post_fiscal_receipt(sinv_doc)
+    post_fiscal_receipt(sinv_doc, method)
 
 
 def autogenerate_vfd(doc, method):
     if doc.is_not_vfd_invoice or doc.vfd_status == "Sent":
         return
     if doc.is_auto_generate_vfd and doc.docstatus == 1:
-        generate_tra_vfd(docname=doc.name, sinv_doc=doc)
+        generate_tra_vfd(docname=doc.name, sinv_doc=doc, method=method)
